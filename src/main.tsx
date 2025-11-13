d5e313c24265e0e788d75280323e3dd26fee6213
v_mykhailov <v.mykhailov@konsmetal.pl>
2025-11-13 14:08:58 +0100

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,7 @@
-// main.jsx or App.jsx
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 
-// Import Vercel Analytics
-import { Analytics } from "@vercel/analytics/react";
+  import { createRoot } from "react-dom/client";
+  import App from "./App.tsx";
+  import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-    <Analytics /> {/* Add this at the root */}
-  </React.StrictMode>
-);
+  createRoot(document.getElementById("root")!).render(<App />);
+  
